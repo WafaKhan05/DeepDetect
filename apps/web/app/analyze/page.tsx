04cd8f1eb19ca4bd7f5962827f951f12ea920f7c
WafaKhan05 <wafa.khan87.80@gmail.com>
2025-05-23 16:49:02 +0530

    added: results on upload page aswell

--- apps/web/app/analyze/page.tsx
+++ apps/web/app/analyze/page.tsx
@@ -1,24 +1,61 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
-import { Upload, AlertCircle } from "lucide-react";
+import { Upload, AlertCircle, Loader2 } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
 import axios from "axios";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
-import { useRouter } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { Badge } from "@/components/ui/badge";
 
 export default function AnalyzePage() {
   const [file, setFile] = useState<File | null>(null);
-  const [analyzing, setAnalyzing] = useState(false);
+  const [uploading, setUplaoding] = useState(false);
   const [progress, setProgress] = useState(0);
   const [message, setMessage] = useState("");
-  const { user } = useKindeBrowserClient();
+  const { user, isLoading } = useKindeBrowserClient();
+  const [isAnalyzing, setIsAnalyzing] = useState(false);
+  const [result, setResult] = useState<{
+    analysis_completed_on: string;
+    confidence: string;
+    id: string;
+    prediction: string;
+    result: string;
+    status: string;
+    userId: string;
+  } | null>(null);
   const [error, setError] = useState(false);
-
   const router = useRouter();
+
+  useEffect(() => {
+    if (isLoading) {
+      return;
+    }
+
+    if (!user) {
+      notFound();
+    }
+
+    if (user.id) {
+      const WebSocketUrl = process.env.NEXT_PUBLIC_WEBSOCKET_ENDPOINT;
+
+      const ws = new WebSocket(`${WebSocketUrl}/${user.id}`);
+
+      ws.onmessage = function (event) {
+        console.log("WebSocket Message:", event.data);
+
+        const data = JSON.parse(event.data);
+
+        if (data.event === "update_status") {
+          setIsAnalyzing(false);
+          setResult(data.data);
+        }
+      };
+    }
+  }, [user, isLoading]);
 
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files[0]) {
@@ -28,7 +65,8 @@ export default function AnalyzePage() {
 
   const handleAnalyze = async () => {
     if (!file || !user) return;
-    setAnalyzing(true);
+    setUplaoding(true);
+    setIsAnalyzing(true);
     // Simulate analysis progress
 
     const formData = new FormData();
@@ -61,7 +99,7 @@ export default function AnalyzePage() {
       setMessage("Upload failed!");
       setError(true);
     }
-    setAnalyzing(false);
+    setUplaoding(false);
   };
 
   return (
@@ -96,20 +134,28 @@ export default function AnalyzePage() {
               ) : (
                 <div className="space-y-4">
                   <p className="text-lg font-medium">{file.name}</p>
-                  <Button onClick={() => setFile(null)} variant="outline">
+                  <Button
+                    onClick={() => {
+                      setFile(null);
+                      setResult(null);
+                      setMessage("");
+                      setProgress(0);
+                    }}
+                    variant="outline"
+                  >
                     Remove
                   </Button>
                 </div>
               )}
             </div>
 
-            {file && !analyzing && (
+            {file && !uploading && (
               <Button className="w-full mt-4" onClick={handleAnalyze}>
                 Start Analysis
               </Button>
             )}
 
-            {analyzing && (
+            {uploading && (
               <div className="mt-4 space-y-2">
                 <Progress value={progress} />
                 <p className="text-sm text-center text-muted-foreground">
@@ -123,16 +169,57 @@ export default function AnalyzePage() {
         <Card>
           <CardContent className="p-6">
             <h2 className="text-2xl font-semibold mb-4">Analysis Results</h2>
-            {!analyzing && progress === 100 ? (
+            {!uploading && progress === 100 ? (
               <div className="space-y-4">
-                <div
-                  className={cn("flex items-center gap-2", {
-                    "text-destructive": error,
-                    "text-primary": !error,
-                  })}
-                >
-                  <AlertCircle className="w-5 h-5" />
-                  <span className="font-medium">{message}</span>
+                {result == null && message && (
+                  <div
+                    className={cn("flex items-center gap-2", {
+                      "text-destructive": error,
+                      "text-primary": !error,
+                    })}
+                  >
+                    <AlertCircle className="w-5 h-5" />
+                    <span className="font-medium">{message}</span>
+                  </div>
+                )}
+                <div>
+                  <h6 className="text-xl font-semibold mb-4">
+                    Deepfake Result
+                  </h6>
+                  {isAnalyzing && (
+                    <div className="flex gap-2">
+                      <Loader2 className="animate-spin" /> <p>Analyzing...</p>
+                    </div>
+                  )}
+
+                  {!isAnalyzing && result != null && (
+                    <div>
+                      <h6 className="font-semibold mb-2">Prediction</h6>
+                      {result.prediction ? (
+                        <>
+                          <Badge
+                            variant={
+                              result.prediction?.toLowerCase() === "fake"
+                                ? "destructive"
+                                : "success"
+                            }
+                            className="flex justify-center items-center gap-4 h-6"
+                          >
+                            {result.prediction?.toUpperCase() ?? "-"}
+                          </Badge>
+                          <p className="py-2">
+                            Confidence: {result.confidence}
+                          </p>
+                          <p>
+                            Analysis Completed At:{" "}
+                            {result.analysis_completed_on.split(".")[0]}
+                          </p>
+                        </>
+                      ) : (
+                        "-"
+                      )}
+                    </div>
+                  )}
                 </div>
                 {/* <div className="space-y-2">
                   <p className="text-sm font-medium">Confidence Score: 89%</p>
